b0f168ef1c5fc59f5aeb8b1ba14c81f8fe4ba5e4
bestrada <bestrada@cipsa.com.pe>
2017-03-30 14:19:57 -0500

    3

---
+++ app/src/main/java/com/cipsa/appparquimetro/adapter/recyclerview/RVAdapter.java
@@ -0,0 +1,8 @@
+package com.cipsa.appparquimetro.adapter.recyclerview;
+
+/**
+ * Created by bestrada on 30/03/2017.
+ */
+
+public class RVAdapter {
+}
---
+++ app/src/main/java/com/cipsa/appparquimetro/entities/Usuario.java
@@ -0,0 +1,8 @@
+package com.cipsa.appparquimetro.entities;
+
+/**
+ * Created by bestrada on 30/03/2017.
+ */
+
+public class Usuario {
+}
