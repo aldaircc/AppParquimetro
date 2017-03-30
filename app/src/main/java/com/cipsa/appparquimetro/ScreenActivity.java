0609985b2ae1d9caf60cc2c9865262f5e123e4c7
bestrada <bestrada@cipsa.com.pe>
2017-03-30 13:43:14 -0500

    2

--- app/src/main/java/com/cipsa/appparquimetro/ScreenActivity.java
+++ app/src/main/java/com/cipsa/appparquimetro/ScreenActivity.java
@@ -1,5 +1,6 @@
 package com.cipsa.appparquimetro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,26 @@ public class ScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen);
+        final int totalProgressTime = 50;
+        final Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    int jumpTime = 0;
+
+                    while (jumpTime < totalProgressTime) {
+                        sleep(1000);
+                        jumpTime += 10;
+//                        pbSplachCargando.setProgress(jumpTime);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(ScreenActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timerThread.start();
     }
 }
