68e431d6886b29562b762cc7a054c3cdfe682dda
ycardenas <yjcardenas@cipsa.com.pe>
2017-03-31 10:46:00 -0500

    login

--- app/src/main/java/com/cipsa/appparquimetro/LoginActivity.java
+++ app/src/main/java/com/cipsa/appparquimetro/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.cipsa.appparquimetro;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -25,32 +28,30 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-       /* btLoginIngresar = (Button) findViewById(R.id.btLoginIngresar);
-        etLoginPassword = (EditText) findViewById(R.id.etLoginUsuario);
-        etLoginUsuario = (EditText)findViewById(R.id.etLoginUsuario);
+        btLoginIngresar = (Button) findViewById(R.id.btLoginIngresar);
+        etLoginUsuario = (EditText) findViewById(R.id.etLoginUsuario);
+        etLoginPassword = (EditText)findViewById(R.id.etLoginPassword);
 
         btLoginIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            //validar(etLoginUsuario.getText().toString(),etLoginPassword.getText().toString());
+            validar(etLoginUsuario.getText().toString(),etLoginPassword.getText().toString());
             }
         });
-*/
+
     }
 
     private  void validar(String usu , String pass)
     {
-        String ur = Constants.URL_GET_VAL_USERS +"usu="+ usu +"&contra" + pass;
+        String ur = Constants.URL_GET_VAL_USERS +"usu="+ usu +"&contra=" + pass;
         StringRequest request = new StringRequest(
                 Request.Method.GET,
                 ur,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                            usuarioMo = ((ParquimetroApp)getApplicationContext()).gson.fromJson(response,UsuarioMo.class);
-                            int num = usuarioMo.getValidarUsuarioResults().size();
-                            Log.i("infor","Numero de usuario"+ String.valueOf(num));
 
+                        ver(response);
                     }
                 },
                 new Response.ErrorListener() {
@@ -64,5 +65,18 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    private void ver(String response){
+        usuarioMo = ((ParquimetroApp)getApplicationContext()).gson.fromJson(response,UsuarioMo.class);
+        int num = usuarioMo.getValidarUsuarioResults().size();
+        Log.i("infor","Numero de usuario"+ String.valueOf(num));
+        if (num > 0){
+            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+        }else 
+        {
+            Toast.makeText(this, "Usuario o Password incorrecto", Toast.LENGTH_SHORT).show();
+        }
 
+
+    }
 }
