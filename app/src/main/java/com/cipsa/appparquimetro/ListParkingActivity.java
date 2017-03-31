8b3eb69e5daa543df7078a43fd5a9a26ce6117b9
bestrada <bestrada@cipsa.com.pe>
2017-03-31 11:56:19 -0500

    listar aparcados

--- app/src/main/java/com/cipsa/appparquimetro/ListParkingActivity.java
+++ app/src/main/java/com/cipsa/appparquimetro/ListParkingActivity.java
@@ -1,7 +1,9 @@
 package com.cipsa.appparquimetro;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ListParkingActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class ListParkingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_parking);
+        setToolbar();
+
+    }
+
+    private void setToolbar(){
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab=getSupportActionBar();
+        if(ab!=null){
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
--- app/src/main/java/com/cipsa/appparquimetro/MainActivity.java
+++ app/src/main/java/com/cipsa/appparquimetro/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cipsa.appparquimetro;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setToolbar();
+    }
+
+
+
+    private void setToolbar(){
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab=getSupportActionBar();
+        if(ab!=null){
+            //ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
